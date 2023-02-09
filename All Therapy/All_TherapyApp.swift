9416be77a60b2fa138ba30c32d2b6773af485f35
Muskan Jindal <muskan.jindal@dhani.com>
2023-02-09 23:33:07 +0530

    Initial Commit

---
+++ All Therapy/All_TherapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_TherapyApp.swift
+//  All Therapy
+//
+//  Created by Muskan Jindal on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct All_TherapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
